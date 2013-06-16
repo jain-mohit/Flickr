3c959270d6c4432581184c935c7659cec2927192
Mohit Jain <majainmail@gmail.com>
2013-06-15 23:12:41 -0400

    added more libraries

--- FlickrApp/FlickrWebRequest.h
+++ FlickrApp/FlickrWebRequest.h
@@ -7,7 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "ASIHTTPRequest.h"
 
 @interface FlickrWebRequest : NSObject
+
++ (FlickrWebRequest*) sharedInstance;
 
 @end
--- FlickrApp/FlickrWebRequest.m
+++ FlickrApp/FlickrWebRequest.m
@@ -10,4 +10,17 @@
 
 @implementation FlickrWebRequest
 
+
+
+-(NSString *)serviceAvailable: (ASIHTTPRequest *)request {
+    NSError *error = [request error];
+    if (error || [request responseStatusCode] != 200) {
+        // This will happen probably when server is offline or not reachable
+        //NSString *message = [NSString stringWithFormat: @"Service not Available: %@ %@",[error localizedDescription],[request responseString]];
+        NSString *message = [NSString stringWithFormat: @"Service not Available: %@ %@",[error localizedDescription],@"Evol Was here"];
+        return message;
+    } else {
+        return [request responseString];
+    }
+}
 @end
